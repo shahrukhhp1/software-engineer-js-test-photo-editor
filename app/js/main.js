c02c2cbbf70e5ab22f0990eb13ca11f250b664ea
shahrukhhp1 <shahrukh_hp1@hotmail.com>
2021-10-09 23:37:55 +0500

    movement done , resizing in process

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,9 @@ const AppView = () => {
             <fieldset>
                 <label for="fileSelector">Select an Image file</label>
                 <input type="file" id="fileSelector" />
+                <button id="moveBtn">Move</button>
+                <button id="resizeBtn">Re-Size</button>
+                <label id="info">-</label>
             </fieldset>
         </form>
 
@@ -14,6 +17,26 @@ const AppView = () => {
     // grab DOM elements inside index.html
     const fileSelector = document.getElementById( "fileSelector" );
     const editorCanvas = document.getElementById( "editorCanvas" );
+    const infoLabel = document.getElementById( "info" );
+    const moveBtn = document.getElementById( "moveBtn" );
+    const resizeBtn = document.getElementById( "resizeBtn" );
+    const ctx = editorCanvas.getContext('2d');
+    const img = new Image();
+    const userConfig = { mouseDown : false , moveTool : true , resizeTool : false , lastX : 0, lastY : 0 };
+    const canvas = {
+        width: 0,
+        height: 0,
+        photo : {
+            id:"",
+            width:0,
+            height :0,
+            x:0,
+            y:0,
+            clipX:0,
+            clipY:0,
+            path:""
+        }
+    };
 
     fileSelector.onchange = function( e ) {
         // get all selected Files
@@ -30,24 +53,10 @@ const AppView = () => {
                     const reader = new FileReader();
                     reader.onload = function( e ) {
                         // create HTMLImageElement holding image data
-                        const img = new Image();
+                        
                         img.src = reader.result;
-
-                        const canvas = {
-                            width: 0,
-                            height: 0,
-                            photo : {
-                                id:"",
-                                width:0,
-                                height :0,
-                                x:0,
-                                y:0,
-                                clipX:0,
-                                clipY:0,
-                            }
-                        };
-
-
+                        canvas.photo.path = img.src;
+                        
                         img.onload = function() {
                             // grab some data from the image
                             const width = img.naturalWidth;
@@ -64,28 +73,78 @@ const AppView = () => {
                             canvas.photo.x=0;
                             canvas.photo.y=0;
 
-                            reLocateImage();
-                                                  
+                            reDoImage();             
                         }
-                        // do your magic here...
-
-                        const ctx = editorCanvas.getContext('2d');
-                        const reLocateImage = function()
-                        {
-                            const ctx = editorCanvas.getContext('2d');
-                            ctx.clearRect(0, 0, editorCanvas.width, editorCanvas.height);
-
-                            ctx.drawImage(img, canvas.photo.clipX, canvas.photo.clipY, canvas.photo.width, 
-                                canvas.photo.height, canvas.photo.x, canvas.photo.y, editorCanvas.width,
-                                    editorCanvas.height);     
-                        }
+                        
                     };
                     reader.readAsDataURL( file );
                     // process just one file.
                     return;
 
+                default:
+                    alert("Invalid file type");
             }
         }
+    };
+
+    moveBtn.onclick = function(e){
+        e.target.className= "active";
+        resizeBtn.className = "";
+        userConfig.moveTool = true;
+        userConfig.resizeTool = false;
+    };
+
+    resizeBtn.onclick = function(e){
+        e.target.className= "active";
+        moveBtn.className = "";
+        userConfig.resizeTool = true;
+        userConfig.moveTool = false;
+    };
+
+    editorCanvas.onmousedown = function() {
+        userConfig.mouseDown = true;
+    };
+
+    
+    editorCanvas.onmouseup = function() {
+        userConfig.mouseDown = false;
+    };
+
+    editorCanvas.onmousemove =function(e) {
+        if(userConfig.mouseDown){
+            let x = e.clientX - editorCanvas.offsetLeft;
+            let y = e.clientY - editorCanvas.offsetTop;
+            
+            
+            if(userConfig.moveTool){    
+                canvas.photo.x = x;
+                canvas.photo.y = y;
+                infoLabel.innerHTML = "Position : " + x + " - " + y;
+                reDoImage();
+            }
+            else if(userConfig.resizeTool){
+                if(userConfig.lastX == userConfig.lastY == 0){
+                    userConfig.lastX = x;
+                    userConfig.lastY = y;
+                }else{
+                    canvas.photo.width += x - userConfig.lastX;
+                    canvas.photo.height += y - userConfig.lastY;  
+                    infoLabel.innerHTML = "Size : " + canvas.photo.width + " - " + canvas.photo.height;
+                    reDoImage();
+                }
+            }
+        }
+    };
+
+
+
+    const reDoImage = function()
+    {
+        ctx.clearRect(0, 0, editorCanvas.width, editorCanvas.height);
+
+        ctx.drawImage(img, canvas.photo.clipX, canvas.photo.clipY, canvas.photo.width, 
+            canvas.photo.height, canvas.photo.x, canvas.photo.y, editorCanvas.width,
+                editorCanvas.height);     
     };
 }
 
